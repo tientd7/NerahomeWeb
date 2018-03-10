8f6ceb398f31e6a2ae02b05b69b3a3e26e01a90b
nguyenhung101196 <dunggoilaanh@gmail.com>
2018-03-10 19:57:36 +0700

    fdsdsfsf

--- NeraHomeWebApp/NerahomeWeb/Controllers/HomeController.cs
+++ NeraHomeWebApp/NerahomeWeb/Controllers/HomeController.cs
@@ -20,11 +20,8 @@ namespace NerahomeWeb.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        
 
-            return View();
-        }
+        
     }
 }
