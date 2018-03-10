2dd6c0db3dfa594976924bef0e7ee80f0c476ace
maihieu <https://github.com/tientd7/NerahomeWeb>
2018-03-10 20:00:31 +0700

    Thêm default

--- NeraHomeWebApp/NerahomeWeb/Controllers/DefaultController.cs
+++ NeraHomeWebApp/NerahomeWeb/Controllers/DefaultController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace NerahomeWeb.Controllers
 {
-    public class HomeController : Controller
+    public class DefaultController : Controller
     {
+        // GET: Default
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
