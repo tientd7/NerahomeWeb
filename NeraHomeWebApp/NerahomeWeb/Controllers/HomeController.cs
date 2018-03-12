76e53c98df4bddff4ac4dccf2d3f5859c5e2ffbd
Mai Hieu <Mai Hieu@MAIHIEU-PC>
2018-03-12 21:39:19 +0700

    Menu Thêm Sửa Xóa

--- NeraHomeWebApp/NerahomeWeb/Controllers/HomeController.cs
+++ NeraHomeWebApp/NerahomeWeb/Controllers/HomeController.cs
@@ -20,9 +20,6 @@ namespace NerahomeWeb.Controllers
         {
             return View();
         }
-        public ActionResult News()
-        {
-            return View();
-        }
+
     }
 }
